dd71dcb464d8001d802f4a25365e4d5574dfc48c
Josue Mora <josue@inno-live.com>
2015-12-21 04:07:08 -0800

    Enabled and configured owl carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+    $("#carousel").owlCarousel({
+         // Most important owl features
+        items : 1,
+        itemsCustom : false,
+        itemsDesktop : [1199,1],
+        itemsDesktopSmall : [980,1],
+        itemsTablet: [768,1],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+        singleItem : false,
+        itemsScaleUp : false,
 
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+        rewindSpeed : 1000,
+     
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : false,
+     
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        rewindNav : true,
+        scrollPerPage : false,
+    })
 });
